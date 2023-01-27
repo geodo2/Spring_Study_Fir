206e8fdfbaad74c2d919923c650d86b5b0c977b4
Minsoo Kim <holiday1883@gmail.com>
2023-01-27 09:09:42 +0900

    add swagger ui

--- src/main/java/com/example/testporject/config/SwaggerConfiguration.java
+++ src/main/java/com/example/testporject/config/SwaggerConfiguration.java
@@ -1,37 +1,60 @@
 package com.example.testporject.config;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
 
-@EnableSwagger2
+
+@Configuration
 public class SwaggerConfiguration {
 
+    private static final String API_NAME = "Programmers Spring Boot Application";
+    private static final String API_VERSION = "1.0.0";
+    private static final String API_DESCRIPTION = "프로그래머스 스프링 부트 애플리케이션입니다.";
+
+    // 접속 경로 : http://localhost:8080/swagger-ui/
     @Bean
-    public Docket api(){
+    public Docket api() {
+        Parameter parameterBuilder = (Parameter) new ParameterBuilder()
+                .name(HttpHeaders.AUTHORIZATION)
+                .description("Access Tocken")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> globalParameters = new ArrayList<>();
+        globalParameters.add(parameterBuilder);
+
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()) //아래에서 설정한 api의 정보를 담는 것
+                .globalOperationParameters(globalParameters)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-                //basePackage는 swagger가 REST controller를 스캔할 때 어디를 범위로 스캔할지 정해주는 것
+                .apis(RequestHandlerSelectors.basePackage("com.example.testporject"))
                 .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages( false);
+                .build();
     }
 
-    private ApiInfo apiInfo() {
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Dachan Hub Open API Test with Swagger")
-                .description("설명 부분")
-                .version("1.0.0") //maven(pom.xml)에서 설정한 프로젝트 version
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
                 .build();
     }
 }
